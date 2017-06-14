d6dc91ee92cd56bce3407bed4537276279fc2036
m_shchyhol <mshchygol@gmail.com>
2017-06-14 22:23:21 +0300

    browserify added

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,59 @@
-/**
- * Created by userPC on 13.06.2017.
- */
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+var sourcemaps = require('gulp-sourcemaps');
+
+var vendors = ['jquery'];
 
 gulp.task('pug', function () {
-  gulp.src('./src/templates/*.pug')
+  return gulp.src('./src/templates/*.pug')
     .pipe(pug({
       pretty: true
     }))
     .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./src/templates/**/*.pug', ['pug'])
+gulp.task('build:vendor', function () {
+  var b = browserify({
+    debug: true
+  });
+
+  // require all libs specified in vendors array
+  vendors.forEach(function (lib) {
+    b.require(lib);
+  });
+
+  b.bundle()
+    .pipe(source('vendor.js'))
+    .pipe(buffer())
+    .pipe(streamify(uglify()))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist'))
 });
 
-gulp.task('default', ['pug', 'watch']);
+gulp.task('build:app', function () {
+  return browserify({
+      entries:'./src/js/main.js',
+      debug: true
+    })
+    .external(vendors)
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(buffer())
+    .pipe(streamify(uglify()))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./src/templates/**/*.pug', ['pug']);
+  gulp.watch('./src/js/**/*.js', ['build:app']);
+});
+
+gulp.task('default', ['pug', 'build:app', 'build:vendor', 'watch']);
---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+var $ = require('jquery');
+
+$(document).ready(function () {
+  console.log('It is jquery! after 1111111111111111 vendor !');
+});
+
+function logger() {
+  console.log('lo1212gging !!!');
+}
+//comment
+logger();
