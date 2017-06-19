fac42af1b06eba81f88a06b63f6c6b4f79a3023c
m_shchyhol <mshchygol@gmail.com>
2017-06-19 09:49:15 +0300

    ui update

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,84 @@
 var $ = require('jquery');
 var Masonry = require('masonry-layout');
 
-var masonry = new Masonry('div.page-content', {
-  itemSelector: 'div.product',
-  columnWidth: 'div.product'
-});
-
 $(document).ready(function () {
+  console.log('jquery');
 
-  console.log('jquery')
+  var yellowStar = '<img src="./img/yellow-star.png" class="yellow-star">';
+  var whiteStar = '<img src="./img/white-star.png" class="white-star">';
+
+  function fiveStars() {
+    var i = 0;
+    var images = '';
+    while (i < 5) {
+      images += yellowStar;
+      i++;
+    }
+    return images;
+  }
+
+  function fourStars() {
+    var i = 0;
+    var images = '';
+    while (i < 4) {
+      images += yellowStar;
+      i++;
+    }
+    return images + whiteStar;
+  }
+
+  function threeStars() {
+    var i = 0;
+    var images = '';
+    while (i < 3) {
+      images += yellowStar;
+      i++;
+    }
+    return images + whiteStar + whiteStar;
+  }
+
+  function twoStars() {
+    var i = 0;
+    var images = '';
+    while (i < 2) {
+      images += yellowStar;
+      i++;
+    }
+
+    while (i < 5) {
+      images += whiteStar;
+      i++;
+    }
+
+    return images;
+  }
+
+  function oneStar() {
+    var i = 0;
+    var images = '';
+    while (i < 4) {
+      images += whiteStar;
+      i++;
+    }
+
+    return yellowStar + images;
+  }
+
+
+
+  function appendRating() {
+    $('.five').append(fiveStars());
+    $('.four').append(fourStars());
+    $('.three').append(threeStars());
+    $('.two').append(twoStars());
+    $('.one').append(oneStar());
+  }
+
+  appendRating();
+
+  var masonry = new Masonry('div.page-content', {
+    itemSelector: 'div.product',
+    columnWidth: 'div.product'
+  });
 
 });
