ec9c60d0a253f5e47ff0985d67f36a598a2bd38b
Mykhaylo Shchyhol <mykhaylo.shchyhol@sethq.com>
2017-06-19 18:02:03 +0300

    layout update

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@ var $ = require('jquery');
 var Masonry = require('masonry-layout');
 
 $(document).ready(function () {
-  console.log('jquery');
 
   var yellowStar = '<img src="./img/yellow-star.png" class="yellow-star">';
   var whiteStar = '<img src="./img/white-star.png" class="white-star">';
@@ -74,11 +73,31 @@ $(document).ready(function () {
     $('.one').append(oneStar());
   }
 
+
+  function calculatePadding() {
+    var BLOCK_WIDTH = 310;
+    var MIN_PADDING = 120;
+    var pageContent = $('#main');
+    var pageContentWidth = $('body').width() - MIN_PADDING;
+
+    var columns = parseInt(pageContentWidth / BLOCK_WIDTH);
+    var padding = parseInt((pageContentWidth - columns * BLOCK_WIDTH)/2 + MIN_PADDING/2);
+
+    pageContent.css('padding', '60px ' + padding + 'px 45px');
+  }
+
+  calculatePadding();
   appendRating();
 
-  var masonry = new Masonry('div.page-content', {
+  new Masonry('div.page-content', {
     itemSelector: 'div.product',
     columnWidth: 'div.product'
+  });
+
+  $(window).resize(function() {
+
+    calculatePadding();
+
   });
 
 });
